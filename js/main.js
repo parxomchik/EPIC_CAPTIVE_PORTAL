0ce7e5078c04cc13d53dfba650a78c3dd26d5d79
parxomchik <dmitriy.parkhomenko@icloud.com>
2015-05-14 13:21:11 +0300

    add autorization js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,64 @@
 $( document ).ready(function() {
-    $.ajax({
-        type: "POST",
-        url: "http://10.7.131.134/exampleService/PostWithOnePrim/",
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data) {
-            alert(data);
-        },
-        error: function() {
-            alert("Error");
-        }
+    var data1 =
+    {
+        "input": "1"
+    };
+
+    addData(data1);
+
+    function addData(data)
+    {
+        $.ajax({
+            type: "POST",
+            url: "http://10.7.131.134/exampleService/PostWithOnePrim/",
+            data: JSON.stringify(data),
+            //contentType: "application/json",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                var logins = JSON.parse(data);
+                console.log( logins.Name );
+                $('#user_name option').val(logins.Id);
+                $('#user_name option').text(logins.Name);
+            },
+            error: function() {
+                alert("Error");
+            }
+
+        });
+    }
+    $("#login-btn").click(function submitButton() {
+        var Auth = {
+            login: $('#inputLoginEmail').val(), // this is fours line
+            password: $('#login_password').val()
+        };
+        addData(Auth);
     });
+    $("#submit").click(function registerButton() {
+//
+        var user = {
+            Name: $('#user_name option').text(),
+            id:   $('#user_name option').val(),
+            pass: $('#password').text()
+        };
+        addUser(user);
+    });
+    function addUser(data)
+    {
+        $.ajax({
+            type: "POST",
+            url: "http://10.7.131.134/exampleService/PostWithOnePrim/",
+            data: JSON.stringify(data),
+            //contentType: "application/json",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                alert("Success");
+            },
+            error: function() {
+                alert("Error");
+            }
+
+        });
+    }
 });
