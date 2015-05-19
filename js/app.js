a5e15bf538ddf9fd4dbb940aae49d4eef76ccf1d
parxomchik <dmitriy.parkhomenko@icloud.com>
2015-05-19 16:37:00 +0300

    fix alert

--- js/app.js
+++ js/app.js
@@ -1,17 +1,11 @@
 var app = angular.module('mgcrea.ngStrapDocs', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
 
-app.controller('MainCtrl', function($scope) {
-});
-
 'use strict';
 
 angular.module('mgcrea.ngStrapDocs')
 
-app.controller('DatepickerDemoCtrl', function($scope, $http) {
+app.controller('DatepickerDemoCtrl', function($scope) {
     $scope.selectedDate = new Date();
-    //$scope.selectedDateAsNumber = Date.UTC(1986, 1, 22);
-    // $scope.fromDate = new Date();
-    // $scope.untilDate = new Date();
     $scope.getType = function(key) {
         return Object.prototype.toString.call($scope[key]);
     };
@@ -21,3 +15,46 @@ app.controller('DatepickerDemoCtrl', function($scope, $http) {
     };
 });
 
+app.controller("loginCtrl", function($scope,$http, $alert) {
+    var parol = $alert({
+        title: "Невірний пароль",
+        //content: 'Best check yo self, you\'re not looking too good.',
+        //placement: 'top',
+        type: 'danger',
+        container : "#error_msg",
+        duration : '3',
+        show: false
+
+    });
+    $scope.logins = {}
+    $http.post("http://10.7.131.134/exampleService/PostWithZero/")
+        .success(function (data) {
+
+            $scope.logins = JSON.parse(data);
+        })
+        .error(function (data)
+        {
+            $scope.error = "SUBMIT ERROR";
+        });
+    $scope.login_submit = function(){
+
+        var user_info = {Id:$scope.user_id, Pass:$scope.user_pass }
+        $http.post("http://10.7.131.134/exampleService/UserRegistry/",user_info)
+
+            .success(function (data) {
+                console.log(user_info)
+                console.log(data)
+                if (data === true){
+                    window.location.replace("http://10.7.131.134/servicedocs_angular/client_page.html");
+                }
+                else{
+                    parol.show();
+                }
+
+            })
+            .error(function (data){
+                console.log(user_info)
+                console.log(data)
+            });
+    }
+});
